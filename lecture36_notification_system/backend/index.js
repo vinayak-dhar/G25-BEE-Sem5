ebcc64fe6dd6f06ff87b4f6e7a850a6623e216ab
vinayak-dhar <vinayakdhar003@gmail.com>
2025-11-04 01:07:00 +0530

    handled submit and post and create

--- lecture36_notification_system/backend/index.js
+++ lecture36_notification_system/backend/index.js
@@ -10,7 +10,8 @@ const io = socket(server,{
     }
 });
 const PORT = 5000;
-const path = require("path")
+const path = require("path");
+const { register } = require("module");
 
 app.use(cors({
     origin:"http://localhost:3000",
@@ -22,21 +23,44 @@ app.use(express.static(path.join(__dirname,"public")));
 
 const Users = {};
 // post -> {
-//  author -> user
+//  author -> username
 //  content -> string 
-//  likes -> [user]
+//  likes -> [username]
 //  createdAt -> date
 // }
-const posts = [];
+const Posts = [];
 
 io.on("connection",(client)=>{
   console.log("User 1 connected -> ",client.id);
   
+  // register user 
   client.on("register",(userName) => {
-    Users[userName] = client.id;
+    Users[userName] = socket.id;
   })
 });
 
+
+app.post("/post/create",async(req,res) => {
+  try {
+    const {username,content} = req.body;
+    const post = {
+      author:username,
+      content,
+      likes:[],
+      createdAt:new Date()
+    };
+
+    Posts.unshift(post);
+    res.status(201).json({post:Posts});
+  } 
+  catch(error) {
+    res.status(401).json({message:error.message});
+  }
+});
+
+app.get('/post/all',async(req,res) => {
+  res.status({posts:Posts});
+})
 
 app.get("/", (req, res) => {
   res.send("server running");
--- lecture36_notification_system/frontend/src/app/page.js
+++ lecture36_notification_system/frontend/src/app/page.js
@@ -1,9 +1,14 @@
 "use client"
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
 export default function Home() {
   const [socket,setSocket] = useState(null);
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  const [username,setUserName] = useState("");
+  const [content,setContent] = useState("");
+
 
   // socket client initialise
   useEffect(() => {
@@ -19,9 +24,44 @@ export default function Home() {
     })
   },[socket]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("register",username);
+    setIsLoggedIn(true);
+  }
+
+  if (!isLoggedIn) {
+    return (
+      <div className="h-screen w-full bg-white text-black flex justify-center items-center">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 border rounded-md px-4 py-5">
+          <label form="username">Username</label>
+          <input onChange={(e) => setUserName(e.target.value)} className="border" id="username" placeholder="Enter Name"></input>
+          <button className="border bg-blue-300 rounded-lg">Register</button>
+        </form>
+      </div>
+    );
+  }
+  
+
+  const handlePostCreate = async (e) => {
+    e.preventDefault();
+    let payload = {
+      username,
+      content
+    };
+    let res = await axios.post("http://localhost:5000/post/create",payload);
+  }
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      
+    <div className="h-screen w-full bg-white text-black px-20 py-10">
+      <h1 className="text-2xl font-semibold">Hello {username}</h1>
+
+      <form onSubmit={handlePostCreate} className="flex flex-col gap-2 border rounded-md px-4 py-5">
+          <label form="username">Create Tweet</label>
+          <textarea rows={3} cols={7} onChange={(e) => setUserName(e.target.value)} className="border" id="username"></textarea>
+          <button className="border bg-blue-300 rounded-lg">Post</button>
+        </form>
     </div>
-  );
+  )
 }
